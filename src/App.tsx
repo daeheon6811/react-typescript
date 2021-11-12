9a60871ce55b67958dd12f1b8056a024d3ab881e
PDH <54939319+wow1186@users.noreply.github.com>
2021-11-12 15:34:29 +0900

    #daeheon [ADD] typeScsript state관리 , 리덕스 사용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
+import BasicCounter from './BasicCounter';
+
 import Greetings from './Greetings';
+import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
-  return <Greetings name="Hello" onClick={onClick} />;
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return (
+    <div>
+  <BasicCounter/>
+  <Greetings name="Hello" onClick={onClick}></Greetings>
+  <MyForm onSubmit={onSubmit} />
+  <ReducerSample />;
+    </div>
+  
+);
+
+  
 }
 
 export default App;
